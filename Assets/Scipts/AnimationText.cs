7c3d5eac11785bbd06d8696ffdf386a57494c2c3
GO <panda@GOs-MacBook-Pro.local>
2016-10-06 14:49:25 +0700

    add animation text + update canvas full screen size

---
+++ Assets/Scipts/AnimationText.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AnimationText : MonoBehaviour {
+
+	public Text textArea;
+	public string[] strings;
+	public float speed = 0.1f;
+
+	int stringIndex = 0;
+	int characterIndex = 0;
+
+	void Start () {
+		StartCoroutine (DisplayTimer ());
+	}
+
+	IEnumerator DisplayTimer() {
+		while (1 == 1) {
+			yield return new WaitForSeconds (speed);
+			if (characterIndex > strings [stringIndex].Length) {
+				continue;
+			}
+			textArea.text = strings [stringIndex].Substring (0, characterIndex);
+			characterIndex++;
+		}
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (characterIndex < strings [stringIndex].Length) {
+				characterIndex = strings [stringIndex].Length;
+			} else if (stringIndex < strings.Length) {
+				stringIndex++;
+				characterIndex = 0;
+			}
+		}
+	}
+}
