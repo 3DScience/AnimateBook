d5a0174fc9cbda52131f89489576e4baf649bb6d
GO <panda@GOs-MacBook-Pro.local>
2016-10-07 14:18:21 +0700

    add audio for AnimationText

--- Assets/Scipts/AnimationText.cs
+++ Assets/Scipts/AnimationText.cs
@@ -5,22 +5,25 @@ using System.Collections;
 public class AnimationText : MonoBehaviour {
 
 	public Text textArea;
-	public string[] strings;
 	public float speed = 0.1f;
 	public float waitTime = 2f;
+	public string[] strings;
+	public AudioClip[] audio;
 
 	private float deltaTime = 0;
 	private float endTime = 0;
+	private float timePlay = 0;
 
 	int stringIndex = 0;
 	int characterIndex = 0;
 
 	void Start () {
 		StartCoroutine (DisplayTimer ());
+		timePlay = audio [stringIndex].length;
+		speed = timePlay / strings [stringIndex].Length - 0.02f;
 	}
 
 	IEnumerator DisplayTimer() {
-
 		while (1 == 1) {
 			yield return new WaitForSeconds (speed);
 			if (characterIndex > strings [stringIndex].Length) {
@@ -33,6 +36,11 @@ public class AnimationText : MonoBehaviour {
 
 	void Update () {
 		deltaTime = Time.realtimeSinceStartup;
+		//deltaTime = GetComponent<AudioSource> ().clip.length;
+		if (characterIndex == 0) {
+			GetComponent<AudioSource> ().Stop();
+			GetComponent<AudioSource> ().PlayOneShot (audio [stringIndex]);
+		}
 
 		if (characterIndex == (strings [stringIndex].Length)) {
 			endTime = deltaTime + waitTime;
