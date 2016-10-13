571df7791d907a0eb741e01b28a934dcea020fce
anhc0709g <anhc0709g@gmail.com>
2016-10-13 23:17:59 +0700

    Update TouchEventControler: at the one moment only one gameobject received touch event! Add OnDrop event.

--- Assets/Scipts/TouchEventControler.cs
+++ Assets/Scipts/TouchEventControler.cs
@@ -9,57 +9,52 @@ public class TouchEventControler : MonoBehaviour {
 	void Update() {
         if (Input.touchCount > 0){
             Touch firstTouch= Input.touches[0];
-            Ray ray = Camera.main.ScreenPointToRay(firstTouch.position);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if( firstTouch.phase== TouchPhase.Began)
             {
-                
-                GameObject touchedGameObject = hit.transform.gameObject;
-                if (Debug.isDebugBuild)
-                    Debug.Log("rx touch event on object: " + touchedGameObject.name);
-                if( touchedGameObject.CompareTag("BackGround" ))
+                DebugOnScreen log=DebugOnScreen.addToGameObject(gameObject);
+                log.Log("on touch "+Time.deltaTime);
+                Ray ray = Camera.main.ScreenPointToRay(firstTouch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
                 {
+                    GameObject touchedGameObject = hit.transform.gameObject;
                     if (Debug.isDebugBuild)
-                        Debug.Log("rx touch obj is BackGround: " );
-                    gameObject.SendMessage("OnTouchs", SendMessageOptions.DontRequireReceiver);
+                        Debug.Log("rx touch event on object: " + touchedGameObject.name);
+                    if (touchedGameObject.CompareTag("BackGround"))
+                    {
+                        if (Debug.isDebugBuild)
+                            Debug.Log("rx touch obj is BackGround: ");
+                        //Camera.main.SendMessage("OnTouchs", SendMessageOptions.DontRequireReceiver);
+                        currentTouchGameObject = Camera.main.gameObject;
+
+                    }
+                    else 
+                    {
+                        currentTouchGameObject = touchedGameObject;
+                    }
+                    currentTouchGameObject.SendMessage("OnTouchs", SendMessageOptions.DontRequireReceiver);
+
                 }
                 else
                 {
-                    if (firstTouch.phase == TouchPhase.Began)
-                    {
-                        touchedGameObject.SendMessage("OnTouchDown", SendMessageOptions.DontRequireReceiver);
-                    }
-                    else if (firstTouch.phase == TouchPhase.Moved)
-                    {
-                        touchedGameObject.SendMessage("OnTouchMoved", SendMessageOptions.DontRequireReceiver);
-                    }
-                    else if (firstTouch.phase == TouchPhase.Stationary)
-                    {
-                        touchedGameObject.SendMessage("OnTouchStay", SendMessageOptions.DontRequireReceiver);
-                    }
-                    else if (firstTouch.phase == TouchPhase.Ended)
-                    {
-                        touchedGameObject.SendMessage("OnTouchUp", SendMessageOptions.DontRequireReceiver);
-                    }
-                    else if (firstTouch.phase == TouchPhase.Canceled)
-                    {
-                        touchedGameObject.SendMessage("OnTouchExit", SendMessageOptions.DontRequireReceiver);
-                    }
-                
+                    currentTouchGameObject = null;
+                    //if (Debug.isDebugBuild)
+                    //    Debug.Log("nothing " );
                 }
-                if (currentTouchGameObject != touchedGameObject)
-                {
-                    if (currentTouchGameObject != null)
-                        currentTouchGameObject.SendMessage("OnTouchExit", SendMessageOptions.DontRequireReceiver);
-                    currentTouchGameObject = touchedGameObject;
-                }
-
-
             }
-            else
+            else if(firstTouch.phase == TouchPhase.Ended || firstTouch.phase == TouchPhase.Canceled)
             {
-                //if (Debug.isDebugBuild)
-                //    Debug.Log("nothing " );
+                if (currentTouchGameObject != null)
+                { 
+                    currentTouchGameObject.SendMessage("OnTouchs", SendMessageOptions.DontRequireReceiver);
+                    currentTouchGameObject = null;
+                }
+            }else
+            {
+                if (currentTouchGameObject != null)
+                {
+                    currentTouchGameObject.SendMessage("OnTouchs", SendMessageOptions.DontRequireReceiver);
+                }
             }
         }else
         {
