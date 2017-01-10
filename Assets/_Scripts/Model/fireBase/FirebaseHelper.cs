a1a8ecee73fd7beafd35a08af1e113607addaee6
anhc0709g <anhc0709g@gmail.com>
2017-01-10 15:44:47 +0700

    add script firebase profile

---
+++ Assets/_Scripts/Model/fireBase/FirebaseHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+public class FirebaseHelper  {
+
+    private static FirebaseHelper _instance = null;
+    private FirebaseHelper()
+    {
+
+    }
+    public static FirebaseHelper getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new FirebaseHelper();
+        }
+        return _instance;
+    }
+
+    DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+    public void initFirebase(System.Action callbackWhenDone)
+    {
+        dependencyStatus = FirebaseApp.CheckDependencies();
+        if (dependencyStatus != DependencyStatus.Available)
+        {
+            FirebaseApp.FixDependenciesAsync().ContinueWith(task => {
+                dependencyStatus = FirebaseApp.CheckDependencies();
+                if (dependencyStatus == DependencyStatus.Available)
+                {
+                    InitializeFirebase(callbackWhenDone);
+                }
+                else
+                {
+                    // This should never happen if we're only using Firebase Analytics.
+                    // It does not rely on any external dependencies.
+                    Debug.LogError(
+                        "Could --  not resolve all Firebase dependencies: " + dependencyStatus);
+                }
+            });
+        }
+        else
+        {
+            InitializeFirebase(callbackWhenDone);
+        }
+    }
+    void InitializeFirebase(System.Action callbackWhenDone)
+    {
+        FirebaseApp app = FirebaseApp.DefaultInstance;
+
+        app.SetEditorDatabaseUrl("https://smallworld3d-2ac88.firebaseio.com/");
+        //app.SetEditorP12FileName ("filebaseTest-2e653eef7319.p12");
+        app.SetEditorServiceAccountEmail("smallworld3d-2ac88@appspot.gserviceaccount.com");
+        //app.SetEditorP12Password ("2e653eef7319ed39d40ed0a6370d9d222bbb555a");
+
+        //  _databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        // Debug.Log("InitializeFirebase:  _databaseReference:" + _databaseReference);
+        DebugOnScreen.Log("InitializeFirebase done ....");
+        callbackWhenDone();
+    }
+
+}
---
+++ Assets/_Scripts/Model/fireBase/ProfileFirebase.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Auth;
+using Firebase.Unity.Editor;
+public class ProfileFirebase {
+    private bool initedFirebase=false;
+    private static ProfileFirebase _instance = null;
+    Firebase.Auth.FirebaseAuth auth;
+    private System.Action<bool> stateChangedCallback;
+
+    public FirebaseUser user;
+    private ProfileFirebase()
+    {
+
+    }
+    public void getCurrentUser(System.Action callbackWhenDone)
+    {
+        if (!initedFirebase)
+        {
+            FirebaseHelper.getInstance().initFirebase(() =>
+            {
+                initedFirebase = true;
+                callbackWhenDone();
+            });
+        }else
+        {
+            callbackWhenDone();
+        }
+
+    } 
+
+    public static ProfileFirebase getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new ProfileFirebase();
+        }
+        return _instance;
+    }
+    public void listenLoginStateChange(System.Action<bool> stateChangedCallback)
+    {
+        DebugOnScreen.Log("listenLoginStateChange ....");
+        this.stateChangedCallback = stateChangedCallback;
+        FirebaseHelper.getInstance().initFirebase(() => {
+            DebugOnScreen.Log("init done ....");
+            auth = FirebaseAuth.DefaultInstance;
+            auth.StateChanged += AuthStateChanged;
+        });
+    }
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        DebugOnScreen.Log("AuthStateChanged ....");
+        if (auth.CurrentUser != user)
+        {
+            if (user == null && auth.CurrentUser != null)
+            {
+                DebugOnScreen.Log("Signed in " + auth.CurrentUser.DisplayName);
+            }
+            //else if (user != null && auth.CurrentUser == null)
+            //{
+            //    Debug.Log("Signed out " + user.DisplayName);
+            //}
+            user = auth.CurrentUser;
+            stateChangedCallback(true);
+        }
+        stateChangedCallback(false);
+    }
+
+
+    public void Login(string email, string password,System.Action<Task<Firebase.Auth.FirebaseUser>> resultCallback)
+    {
+        Debug.Log(String.Format("Attempting to sign in as {0}...", email));
+
+        auth.SignInWithEmailAndPasswordAsync(email, password)
+          .ContinueWith(resultCallback);
+    }
+    void HandleSigninResult(Task<Firebase.Auth.FirebaseUser> authTask)
+    {
+
+        if (authTask.IsCanceled)
+        {
+            Debug.Log("SignIn canceled.");
+        }
+        else if (authTask.IsFaulted)
+        {
+            Debug.Log("Login encountered an error.");
+            Debug.Log(authTask.Exception.ToString());
+        }
+        else if (authTask.IsCompleted)
+        {
+            Debug.Log("Login completed.");
+            Debug.Log("Signing out.");
+            auth.SignOut();
+        }
+    }
+
+}
