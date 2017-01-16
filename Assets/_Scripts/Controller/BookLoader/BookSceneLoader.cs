ead810134fe484eed5ddb92b1a1a271a04994c2b
anhc0709g <anhc0709g@gmail.com>
2017-01-16 11:28:46 +0700

    anhnt: add loading indicator when loading book

--- Assets/_Scripts/Controller/BookLoader/BookSceneLoader.cs
+++ Assets/_Scripts/Controller/BookLoader/BookSceneLoader.cs
@@ -7,9 +7,12 @@ public class BookSceneLoader : MonoBehaviour {
     public string assetBundleName;
     public string sceneName;
     // Use this for initialization
+
     IEnumerator Start () {
+        GlobalVar.shareContext.loadingIdicator.SetActive(true);
         yield return AssetBundleHelper.getInstance().LoadScene(assetBundleName, sceneName, true);
         DestroyObject(this);
-	}
+        GlobalVar.shareContext.loadingIdicator.SetActive(false);
+    }
 
 }
--- Assets/_Scripts/Global/ShareContext.cs
+++ Assets/_Scripts/Global/ShareContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShareContext : MonoBehaviour {
-
+    public GameObject loadingIdicator;
 	public Dictionary<string, System.Object> shareVar = new Dictionary<string, System.Object>();
 	// Use this for initialization
 	void Start () {
         DontDestroyOnLoad(this);
-	}
+        loadingIdicator = Instantiate(Resources.Load("Prefabs/loaddingIndicator", typeof(GameObject)),gameObject.transform) as GameObject;
+        loadingIdicator.SetActive(false);
+    }
 	
 }
