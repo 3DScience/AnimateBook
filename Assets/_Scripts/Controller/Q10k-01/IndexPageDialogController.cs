ed5711d84c1bf95a7001fecd85cbd45ed5467e24
VinhNH <vinhmbgame@gmail.com>
2017-02-09 09:52:32 +0700

    add keySmallWorld.keystore, Drop index menu for 2D Books

---
+++ Assets/_Scripts/Controller/Q10k-01/IndexPageDialogController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+public class IndexPageDialogController : MonoBehaviour {
+
+	public GameObject dialogUi;
+
+	void Start () {
+		dialogUi.SetActive(false);
+		DontDestroyOnLoad(gameObject.transform.parent.gameObject);
+		//ProfileFirebase.getInstance().listenLoginStateChange(stateChangedCallback);
+
+	}
+
+	public void OnSettingClick()
+	{
+		Debug.Log("OnMouseDown");
+		if ( !dialogUi.activeSelf)
+		{
+			dialogUi.SetActive(true);
+		}
+	}
+
+
+	void Update () {
+
+	}
+
+	public void onCloseButtonClick()
+	{
+		dialogUi.SetActive(false);
+	}
+
+	public void OnEdgeClick()
+	{
+		Debug.Log("OnEdgeClick");
+		dialogUi.SetActive(false);
+	}
+
+}
