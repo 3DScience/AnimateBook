26f866674b68f88f2665303e8e03eb48e1230e3a
anhc0709g <anhc0709g@gmail.com>
2017-02-17 11:05:17 +0700

    anhnt: update script bookloader.

--- Assets/_Scripts/Util/AssetBundleHelper.cs
+++ Assets/_Scripts/Util/AssetBundleHelper.cs
@@ -29,18 +29,20 @@ public class AssetBundleHelper {
         // With this code, when in-editor or using a development builds: Always use the AssetBundle Server
         // (This is very dependent on the production workflow of the project. 
         // 	Another approach would be to make this configurable in the standalone player.)
-//        #if DEVELOPMENT_BUILD
-//                        AssetBundleManager.SetDevelopmentAssetBundleServer();
-//        #else
-//                // Use the following code if AssetBundles are embedded in the project for example via StreamingAssets folder etc:
-//                //AssetBundleManager.SetSourceAssetBundleURL("http://192.168.0.201/unity3d/3dbook_test/");
-//                AssetBundleManager.SetSourceAssetBundleURL("file://" + GlobalVar.DATA_PATH + "/" + assetBundleName + "/");
-//                // Or customize the URL based on your deployment or configuration
-//                //AssetBundleManager.SetSourceAssetBundleURL("http://www.MyWebsite/MyAssetBundles");
-//        #endif
-
+        //        #if DEVELOPMENT_BUILD
+        //                        AssetBundleManager.SetDevelopmentAssetBundleServer();
+        //        #else
+        //                // Use the following code if AssetBundles are embedded in the project for example via StreamingAssets folder etc:
+        //                //AssetBundleManager.SetSourceAssetBundleURL("http://192.168.0.201/unity3d/3dbook_test/");
+        //                AssetBundleManager.SetSourceAssetBundleURL("file://" + GlobalVar.DATA_PATH + "/" + assetBundleName + "/");
+        //                // Or customize the URL based on your deployment or configuration
+        //                //AssetBundleManager.SetSourceAssetBundleURL("http://www.MyWebsite/MyAssetBundles");
+        //        #endif
+#if !UNITY_WEBGL
 		AssetBundleManager.SetSourceAssetBundleURL("file://" + GlobalVar.DATA_PATH + "/" + assetBundleName + "/");
-
+#else
+        AssetBundleManager.SetSourceAssetBundleURL("file://" + GlobalVar.BASE_ASSET_URL + "/" + assetBundleName );
+#endif
         this.assetBundleName=assetBundleName;
         // Initialize AssetBundleManifest which loads the AssetBundleManifest object.
         var request = AssetBundleManager.Initialize();
